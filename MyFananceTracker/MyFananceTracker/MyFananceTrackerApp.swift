4dce2c59da8e2211a5586ee35041355eed193799
Vitya Mandryk <vityamandryk69@icloud.com>
2024-12-16 14:18:34 +0200

    fix
    fixes

--- MyFananceTracker/MyFananceTracker/MyFananceTrackerApp.swift
+++ MyFananceTracker/MyFananceTracker/MyFananceTrackerApp.swift
@@ -8,11 +8,16 @@
 import SwiftUI
 import UIKit
 import FBSDKCoreKit
-
+import FacebookCore
+import FacebookAEM
+import AppTrackingTransparency
+import AdSupport
 
 @main
-struct MyFananceTrackerApp: App {
+struct MyFinanceTrackerApp: App {
     @StateObject private var viewModel = WebViewModel()
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
         WindowGroup {
@@ -24,8 +29,46 @@ struct MyFananceTrackerApp: App {
                 }
             }
             .onAppear {
+                requestTrackingPermission()
                 viewModel.fetchWebViewData()
             }
         }
+    }
+    
+    private func requestTrackingPermission() {
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization { status in
+                switch status {
+                case .authorized:
+                    print("Tracking authorized.")
+                case .denied:
+                    print("Tracking denied.")
+                case .restricted:
+                    print("Tracking restricted.")
+                case .notDetermined:
+                    print("Tracking not determined.")
+                @unknown default:
+                    print("Unknown tracking status.")
+                }
+            }
+        } else {
+            print("ATT is not available on this iOS version.")
+        }
+    }
+}
+
+// MARK: - AppDelegate for Facebook SDK Integration
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application( _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        return true
+    }
+
+    func application( _ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        return ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation])
     }
 }
